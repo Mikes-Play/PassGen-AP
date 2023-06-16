31444b12cebb575f9bbe1bf8795bd1c856fc1f28
Mike's Play <zerpamiqueas2019@gmail.com>
2023-06-16 18:00:02 -0300

    TODO Today

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import string
 import secrets
+
+#TODO Crear menu de seleccion de:
+#* Alfabetos a utilizar
+#* Longitud de Contraseña
+
+#TODO Reordenar y estilizar alphabet y strings
 alphabet = string.ascii_letters + string.digits
+
+#TODO Reemplazar por "inputs"
 password = ''.join(secrets.choice(alphabet) for i in range(8))
+
